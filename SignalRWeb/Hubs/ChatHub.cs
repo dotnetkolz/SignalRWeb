7cc6d90448bc6354d30cc7ac712bf36231e3d07e
Nikhil <nikhil_koli@cable.comcast.com>
2019-01-03 22:48:57 -0500

    Initial commit

---
+++ SignalRWeb/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWeb.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            var hubcontext = Context;
+            var grp = Groups;
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
