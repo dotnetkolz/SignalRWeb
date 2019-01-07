29388fcfd6c730c9c72fe9ea4b68740c8092d016
DotNetKolz <dotnetkolz@gmail.com>
2019-01-06 23:46:23 -0500

    ok

--- SignalRWeb/Hubs/ChatHub.cs
+++ SignalRWeb/Hubs/ChatHub.cs
@@ -9,8 +9,31 @@ namespace SignalRWeb.Hubs
         public async Task SendMessage(string user, string message)
         {
             var hubcontext = Context;
+            var httpContext = hubcontext.GetHttpContext();
+
             var grp = Groups;
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task OnlyMessage(string message)
+        {
+            var hubcontext = Context;
+            var grp = Groups;
+            await Clients.All.SendAsync("ReceiveOnlyMessage", message);
+        }
+    }
+
+    public class BatHub : Hub
+    {
+        public async Task SendMessage(Data data)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", data);
+        }
+    }
+
+    public class Data
+    {
+        public string Name { get; set; }
+        public string message { get; set; }
     }
 }
